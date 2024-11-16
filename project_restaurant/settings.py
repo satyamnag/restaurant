adc1eca8f71731f65ffdae3f8bb5762ce89a0a46
SATYAM NAG <satyamnag.k@gmail.com>
2024-11-16 23:55:31 +0530

    first commit

--- project_restaurant/settings.py
+++ project_restaurant/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'project_restaurant.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'restaurant_db',
+        'USER': 'postgres',
+        'PASSWORD': 'idonotknow',
+        'HOST': 'localhost',
     }
 }
 
@@ -116,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT=BASE_DIR /'static'
+STATICFILES_DIRS=[
+    'project_restaurant/static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
