72376c75f012717a8d5bdbf333613d5958f8c758
SATYAM NAG <satyamnag.k@gmail.com>
2024-11-19 15:09:26 +0530

    vendor registration

--- app_accounts/views.py
+++ app_accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import *
 from .forms import *
 from .models import *
 from django.contrib import messages
+from vendor.forms import *
 
 # Create your views here.
 def registerUser(request):
@@ -44,4 +45,39 @@ def registerUser(request):
     return render(request, 'app_accounts/registerUser.html', context)
 
 def registerRestaurant(request):
-    return render(request, 'app_accounts/registerRestaurant.html')
+    if request.method=='POST':
+        form=UserForm(request.POST)
+        vendor_form=VendorForm(request.POST, request.FILES)
+        if form.is_valid() and vendor_form.is_valid():
+            first_name=form.cleaned_data['first_name']
+            last_name=form.cleaned_data['last_name']
+            username=form.cleaned_data['username']
+            email=form.cleaned_data['email']
+            password=form.cleaned_data['password']
+            user=User.objects.create_user(
+                first_name=first_name,
+                last_name=last_name,
+                username=username,
+                email=email,
+                password=password,
+            )
+            user.role=User.RESTAURANT
+            user.save()
+            vendor=vendor_form.save(commit=False)
+            vendor.user=user
+            user_profile=UserProfile.objects.get(user=user)
+            vendor.user_profile=user_profile
+            vendor.save()
+            messages.success(request, 'Your account has been registered successfully! Please wait for the approval.')
+            return redirect('registerRestaurant')
+        else:
+            form.errors
+
+    else:
+        form=UserForm()
+        vendor_form=VendorForm()
+    context={
+        'form':form,
+        'vendor_form':vendor_form,
+    }
+    return render(request, 'app_accounts/registerRestaurant.html', context)
